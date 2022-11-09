5190b46b7b1599603f5071e1c8ec6c523f7e0f8a
forcejie <2963301696@qq.com>
2022-11-09 11:31:52 +0800

    轮播、区域组件

--- pages/main_music/main_music.js
+++ pages/main_music/main_music.js
@@ -1,25 +1,41 @@
 // pages/main_music/main_music.js
 import { getMusicBanner } from "../../services/music"
+import querySelect from "../../utils/query-select"
+// import throttle from "../../utils/throttle"
+import { throttle } from 'underscore'
+
+
+const querySelectThrottle = throttle(querySelect, 100)
+// const app = getApp()
 Page({
   data: {
     searchValue: "",
-    banners:[]
+    banners:[],
+    bannerHeight: 150,
+    screenWidth: 375,
   },
-  onload(){
-    getMusicBanner().then(res => {
-      this.setData({ banners: res.banners })
-    })
-    // this.fetchMusicBanner()
+  onLoad(){
+    // getMusicBanner().then(res => {
+    //   this.setData({ banners: res.banners })
+    // })
+    this.fetchMusicBanner()
   },
     // 网络请求的方法封装
-    // async fetchMusicBanner() {
-    //   const res = await getMusicBanner()
-      // this.setData({ banners: res.banners })
+     async fetchMusicBanner() {
+       const res = await getMusicBanner()
+       this.setData({ banners: res.banners })
     //   console.log(res)
-    // },
+     },
 
   onSearchClick(){
     wx.navigateTo({url: '/pages/detail_search/detail_search',})
   },
-  
+  onBannerImageLoad(event) {
+    querySelectThrottle(".banner-image").then(res => {
+      this.setData({ bannerHeight: res[0].height })
+    })
+  },
+  onRecommendClick(){
+    console.log(11)
+  }
 })
